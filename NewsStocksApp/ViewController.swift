16243a6b592b98cc86ca82d443aa1a57faa70a8a
AntonTuzov <antontuzovf@gmail.com>
2021-08-20 09:54:51 +0300

    Initial Commit

---
+++ NewsStocksApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsStocksApp
+//
+//  Created by Anton Tuzov on 20.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
