3a8380527de9d987f42b4a57d983d2839bbf87bf
AntonTuzov <antontuzovf@gmail.com>
2021-08-25 10:17:48 +0300

    Title view

--- NewsStocksApp/Controllers/WatchListViewController.swift
+++ NewsStocksApp/Controllers/WatchListViewController.swift
@@ -7,16 +7,9 @@
 
 import UIKit
 
-class WatchListViewController: UIViewController, SearchResultViewControllerDelegate,
-                               UISearchResultsUpdating {
-    func updateSearchResults(for searchController: UISearchController) {
-        print("ff")
-    }
-    
-    func searchResultViewControllerDidSelect(searchResult: SResult) {
-        print("pp")
-    }
+class WatchListViewController: UIViewController {
   
+    
 
     
     
@@ -25,9 +18,28 @@ class WatchListViewController: UIViewController, SearchResultViewControllerDeleg
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         setUpSerchController()
-        
+        setupTitleView()
         
     }
+    
+    
+    private func setupTitleView() {
+        
+        let titleView = UIView(frame: CGRect(x: 0, y: 0,
+                                                     width: view.width,
+                                                     height: navigationController?.navigationBar.height ?? 100 ))
+                
+                titleView.backgroundColor = .systemBackground
+                navigationItem.titleView = titleView
+                
+                let lable = UILabel(frame: CGRect(x: 10, y: 0, width: titleView.width - 40, height: titleView.height))
+                lable.text = "Stocks News"
+                lable.font = .systemFont(ofSize: 32, weight: .medium)
+                titleView.addSubview(lable)
+        
+    }
+    
+    
     
     private func setUpSerchController(){
             let resultVC = SearchResultViewController()
@@ -39,3 +51,32 @@ class WatchListViewController: UIViewController, SearchResultViewControllerDeleg
 
 }
 
+extension WatchListViewController:  UISearchResultsUpdating {
+    
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let query = searchController.searchBar.text,
+              let resultVC = searchController.searchResultsController as?
+                SearchResultViewController,
+              !query.trimmingCharacters(in: .whitespaces).isEmpty else {
+            
+            return
+        }
+        print(query)
+    }
+    
+    
+    
+}
+
+
+extension WatchListViewController: SearchResultViewControllerDelegate {
+    
+    func searchResultViewControllerDidSelect(searchResult: SResult) {
+       
+    }
+  
+    
+    
+    
+}
